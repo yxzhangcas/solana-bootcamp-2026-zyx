f8b60611f8c44222ea7871ddf56b3e46df4ceaf7
yxzhangcas <yxzhangcas@outlook.com>
2026-06-23 14:16:44 +0800

    [escrow] init project

---
+++ 04-escrow/escrow/programs/escrow/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("CBm7pwVU1tcwBgWtHyJaEF5qeJkns6iQSvL2A6wkcrxo");
+
+#[program]
+pub mod escrow {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
