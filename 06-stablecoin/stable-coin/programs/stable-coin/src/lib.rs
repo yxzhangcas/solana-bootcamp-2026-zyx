f715d11daa73d496f58ec0c67a1c4063a00caf6b
yxzhangcas <yxzhangcas@outlook.com>
2026-07-20 07:54:21 +0800

    [stable-coin] init program

---
+++ 06-stablecoin/stable-coin/programs/stable-coin/src/lib.rs
@@ -0,0 +1,25 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("6ddbQXeryFeCtwuNi6H2dK1gEvDWZYiNYe13xd5gZo1s");
+
+#[program]
+pub mod stable_coin {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        crate::instructions::initialize::handle_initialize(ctx)
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        crate::instructions::increment::handle_increment(ctx)
+    }
+}
